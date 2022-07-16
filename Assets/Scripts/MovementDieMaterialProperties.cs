6861069daa3853283f06f9f5e6116838839c5dcd
Anthony Diggle <fuseinabowl@gmail.com>
2022-07-16 21:37:51 +0100

    Add ability to change the colour of dice glow during the game

---
+++ Assets/Scripts/MovementDieMaterialProperties.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+[ExecuteAlways]
+public class MovementDieMaterialProperties : MonoBehaviour
+{
+    [SerializeField]
+    private Renderer targetRenderer = null;
+
+    [SerializeField]
+    [ColorUsage(false, true)]
+    private Color glowColor = Color.white;
+
+    private MaterialPropertyBlock propertyBlock = null;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(targetRenderer);
+    }
+
+    private void Start()
+    {
+        propertyBlock = new MaterialPropertyBlock();
+    }
+
+    private void Update()
+    {
+        Assert.IsNotNull(propertyBlock);
+        propertyBlock.SetColor("_GlowColor", glowColor);
+        for (var materialIndex = 0; materialIndex < targetRenderer.sharedMaterials.Length; ++materialIndex)
+        {
+            targetRenderer.SetPropertyBlock(propertyBlock, materialIndex);
+        }
+    }
+}
